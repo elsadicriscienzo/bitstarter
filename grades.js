b1abd853c0bb4dcf408b02d6b69cb4caa12a64df
Ubuntu <ubuntu@ip-172-31-34-9.us-west-2.compute.internal>
2013-07-07 18:58:13 +0000

    Update grades.js

--- grades.js
+++ grades.js
@@ -65,7 +65,14 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url', 'Url to html file')
         .parse(process.argv);
+    if(program.url){
+	var url = program.url.toString(); 
+	rest.get(url).on('complete', function(result, response){  
+		console.log(result);
+	});}
+
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
